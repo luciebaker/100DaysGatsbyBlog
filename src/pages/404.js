beafd346688a29b7d0f3139f6cbba88976603ec8
luciebaker <luciebdev@gmail.com>
2020-01-06 10:09:58 -0500

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
 import React from "react"
+import NotFound from '../images/notFound.png'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <SEO title="404: Not found" />
+  <div className="container text-center">
+    <img className="notfound-img" src={NotFound} alt="Page Not Found"/>
+    </div>
+    <div className="container pb-4 text-center">
+    <h1 className="notfound-text">You just found a little corner of the web that doesn't exist...</h1>
+    <p>Don't worry... Just use the Navigation at the top of your screen to find your way back.</p>
+    </div>
   </Layout>
 )
+
 
 export default NotFoundPage
